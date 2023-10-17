d50a553bd4eb8b9dd86661d55f0e5f2e270cdfa8
Ceyda <ceydaceyhan2001@gmail.com>
2023-10-17 14:07:27 +0300

    Game Over ekranı yapıldı, farklı platform ve cihazlarda çalışması sağlandı.

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StyleSheet, ImageBackground } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
+<<<<<<< HEAD
 
 export default function App() {
   return (
@@ -8,12 +9,85 @@ export default function App() {
       <ImageBackground
         source={require("./assets/images/background.png")}
         resizeMode="cover"
+=======
+// import { useFonts } from "expo-font";
+// import * as SplashScreen from "expo-splash-screen";
+import GameScreen from "./screens/GameScreen";
+import Colors from "./constants/colors";
+import GameOverScreen from "./screens/GameOverScreen";
+import { StatusBar } from "expo-status-bar";
+
+export default function App() {
+  const [userNumber, setUserNumber] = useState();
+  const [gameIsOver, setGameIsOver] = useState(true);
+  const [guessRounds, setGuessRounds] = useState(0);
+
+  // const [fontsLoading] = useFonts({
+  //   "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+  //   "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  // });
+
+  // if (!fontsLoading) {
+  //   return SplashScreen.preventAutoHideAsync();
+  // }
+
+  function pickedNumberHandler(pickedNumber) {
+    setUserNumber(pickedNumber);
+    setGameIsOver(false);
+  }
+
+  function gameOverHandler(numberOfRounds) {
+    setGameIsOver(true);
+    setGuessRounds(numberOfRounds);
+  }
+
+  function startNewGameHandler() {
+    setUserNumber(null);
+    setGuessRounds(0);
+  }
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
+
+  if (userNumber) {
+    screen = (
+      <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />
+    );
+  }
+
+  if (gameIsOver && userNumber) {
+    screen = (
+      <GameOverScreen
+        userNumber={userNumber}
+        roundsNumber={guessRounds}
+        onStartNewGame={startNewGameHandler}
+      />
+    );
+  }
+
+  return (
+    <>
+      <StatusBar style="light" />
+      <LinearGradient
+        colors={[Colors.primary700, Colors.accent500]}
+>>>>>>> parent of c2bea7a... Game Over ekranı yapıldı, farklı platform ve cihazlarda çalışması sağlandı.
         style={styles.rootScreen}
-        imageStyle={styles.backgroundImage}
       >
+<<<<<<< HEAD
         <StartGameScreen />
       </ImageBackground>
     </LinearGradient>
+=======
+        <ImageBackground
+          source={require("./assets/images/background.png")}
+          resizeMode="cover"
+          style={styles.rootScreen}
+          imageStyle={styles.backgroundImage}
+        >
+          <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
+        </ImageBackground>
+      </LinearGradient>
+    </>
+>>>>>>> parent of c2bea7a... Game Over ekranı yapıldı, farklı platform ve cihazlarda çalışması sağlandı.
   );
 }
 
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { StyleSheet, TextInput, View } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
@@ -17,17 +18,97 @@ function StartGameScreen() {
         </View>
         <View style={styles.buttonContainer}>
           <PrimaryButton>Confirm</PrimaryButton>
+=======
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Alert,
+  useWindowDimensions,
+  KeyboardAvoidingView,
+  ScrollView,
+} from "react-native";
+import { useState } from "react";
+import Colors from "../constants/colors";
+import PrimaryButton from "../components/ui/PrimaryButton";
+import Title from "../components/ui/Title";
+import Card from "../components/ui/Card";
+import InstructionText from "../components/ui/InstructionText";
+
+function StartGameScreen({ onPickNumber }) {
+  const [enteredNumber, setEnteredNumber] = useState("");
+
+  const { width, height } = useWindowDimensions();
+
+  function numberInputHandler(enteredText) {
+    setEnteredNumber(enteredText);
+  }
+
+  function resetInputHandler() {
+    setEnteredNumber("");
+  }
+
+  function confirmInputHandler() {
+    const chosenNumber = parseInt(enteredNumber);
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        "Invalid Number!",
+        "Number has to be a number between 1 and 99.",
+        [{ text: "Okay", style: "destructive", onPress: resetInputHandler }]
+      );
+      return;
+    }
+    onPickNumber(chosenNumber);
+  }
+
+  const marginTopDistance = height < 380 ? 30 : 100;
+
+  return (
+    <ScrollView style={styles.screen}>
+      <KeyboardAvoidingView style={styles.screen} behavior="position">
+        <View style={[styles.rootContainer, { marginTop: marginTopDistance }]}>
+          <Title>Guess My Number</Title>
+          <Card>
+            <InstructionText>Enter a Number</InstructionText>
+            <TextInput
+              style={styles.numberInput}
+              maxLength={2}
+              keyboardType="number-pad"
+              autoCapitalize="none"
+              autoCorrect={false}
+              onChangeText={numberInputHandler}
+              value={enteredNumber}
+            />
+            <View style={styles.buttonsContainer}>
+              <View style={styles.buttonContainer}>
+                <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+              </View>
+              <View style={styles.buttonContainer}>
+                <PrimaryButton onPress={confirmInputHandler}>
+                  Confirm
+                </PrimaryButton>
+              </View>
+            </View>
+          </Card>
+>>>>>>> parent of c2bea7a... Game Over ekranı yapıldı, farklı platform ve cihazlarda çalışması sağlandı.
         </View>
-      </View>
-    </View>
+      </KeyboardAvoidingView>
+    </ScrollView>
   );
 }
 export default StartGameScreen;
 
+// const deviceHeight = Dimensions.get("window").height;
+
 const styles = StyleSheet.create({
-  inputContainer: {
-    justifyContent: "center",
+  screen: {
+    flex: 1,
+  },
+  rootContainer: {
+    flex: 1,
+    // marginTop: deviceHeight < 380 ? 30 : 100,
     alignItems: "center",
+<<<<<<< HEAD
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
@@ -38,6 +119,8 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+=======
+>>>>>>> parent of c2bea7a... Game Over ekranı yapıldı, farklı platform ve cihazlarda çalışması sağlandı.
   },
   numberInput: {
     height: 50,
